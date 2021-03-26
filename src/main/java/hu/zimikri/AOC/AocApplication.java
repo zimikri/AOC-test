4a3696d0f15b2c85febd19ec720bbf8a4f931aa6
Zimay Krisztián <zimay.krisztian@gmail.com>
2021-03-27 00:16:42 +0100

    style(AocApplication): fix indentation

--- src/main/java/hu/zimikri/AOC/AocApplication.java
+++ src/main/java/hu/zimikri/AOC/AocApplication.java
@@ -5,25 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AocApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		// Run with given example data
-		try {
-			BridgeCrossing bridgeCrossing = new BridgeCrossing();
+    // Run with given example data
+    try {
+      BridgeCrossing bridgeCrossing = new BridgeCrossing();
 
       System.out.println("Whith given data [10, 5, 2, 1]:");
-			System.out.println(bridgeCrossing.findShortestTime("10, 5, 2, 1").toString());
+      System.out.println(bridgeCrossing.findShortestTime("10, 5, 2, 1").toString());
       System.out.println();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
 
-		// Read data from console
-		try {
-			BridgeCrossing bridgeCrossing = new BridgeCrossing();
-			System.out.println(bridgeCrossing.findShortestTime().toString());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    // Read data from console
+    try {
+      BridgeCrossing bridgeCrossing = new BridgeCrossing();
+      System.out.println(bridgeCrossing.findShortestTime().toString());
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
