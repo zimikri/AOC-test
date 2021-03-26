86ebe45658d69a5f883d22e48f9bb9a11204ba5e
Zimay Krisztián <zimay.krisztian@gmail.com>
2021-03-26 16:12:38 +0100

    initial commit

---
+++ src/test/java/hu/zimikri/AOC/AocApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zimikri.AOC;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
